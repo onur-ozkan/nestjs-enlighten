d666f0547d3ca54fd7d34b60c95e9e54587b0edb
ozkanonur <onurozkan.dev@outlook.com>
2020-02-25 23:39:52 +0300

    Catch the errors by Filter way

---
+++ src/Filters/http-exception.filter.ts
@@ -0,0 +1,22 @@
+
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<any>();
+    const request = ctx.getRequest<any>();
+    const status = exception.getStatus();
+
+    console.log(status)
+
+    response
+      .status(status)
+      .json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      });
+  }
+}
