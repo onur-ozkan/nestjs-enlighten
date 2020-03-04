f50b261ab2123153090a446ab442ba0f1443637a
ozkanonur <onurozkan.dev@outlook.com>
2020-03-04 11:13:13 +0300

    Render compiled view from service class

--- src/Filters/exception.filter.ts
+++ src/Filters/exception.filter.ts
@@ -1,23 +1,13 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
-import * as fs from 'fs';
-import * as ejs from 'ejs';
+import { ViewCompilerService } from '../Services/ViewCompiler'
 
 @Catch()
 export class ExceptionFilter extends BaseExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
+    const response = host.switchToHttp().getResponse<any>();
 
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<any>();
-    const request = ctx.getRequest<any>();
-
-    const stylingRaw = fs.readFileSync(`${__dirname}/../../assets/style/index.css`, 'utf8');
-    const compiledView = ejs.render(fs.readFileSync(`${__dirname}/../views/index.ejs`, 'utf8'), {
-      response: exception.response,
-      baseUrl: `${request.protocol}://${request.headers.host}/`,
-      stylingRaw,
-    });
-
+    const compiledView = new ViewCompilerService(host, exception).getCompiledView();
     response.status(response.statusCode).send(compiledView);
   }
 }
