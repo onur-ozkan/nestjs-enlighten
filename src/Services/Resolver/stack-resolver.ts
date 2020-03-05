7f96ba45ad46418345ab7c48934dad1c5fd74832
ozkanonur <onurozkan.dev@outlook.com>
2020-03-05 18:01:22 +0300

    Resolve the exception line and add it to stack object as property.

--- src/Services/Resolver/stack-resolver.ts
+++ src/Services/Resolver/stack-resolver.ts
@@ -1,4 +1,6 @@
 import * as stackTrace from 'stack-trace'
+import { ErrorStack } from '../../@types'
+import * as fs from 'fs'
 
 export class StackResolverService {
 	private exception: any
@@ -7,10 +9,16 @@ export class StackResolverService {
 		this.exception = exception
 	}
 
-	public async getProperErrorStack(): Promise<object> {
-		const errorStack = await stackTrace.parse(this.exception)
+	public async getProperErrorStack(): Promise<ErrorStack> {
+		let errorStack = await stackTrace.parse(this.exception)
 
-		errorStack.map((_stack, index) => {
+		await errorStack.map(async (_stack, index) => {
+			// Resolve the exception line and add it to stack object as property.
+			const data = fs.readFileSync(errorStack[index].fileName, 'utf-8')
+			const lines = data.split(/\r?\n/);
+			errorStack[index].codeRaw = lines.slice((errorStack[index].lineNumber - 15) >= 0 ? errorStack[index].lineNumber - 15 : 0, errorStack[index].lineNumber + 15)
+
+			// Update absolute paths with relative ones.
 			const relativeFileName = errorStack[index].getFileName().indexOf(process.cwd())
 			if (relativeFileName > -1) {
 				errorStack[index].fileName = errorStack[index].getFileName().replace(process.cwd(), '').replace(/\\|\//, '')
