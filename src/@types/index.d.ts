1369a5d923e6fafe8dfd815fcf6b07fd1682a7f1
ozkanonur <onurozkan.dev@outlook.com>
2020-03-05 17:59:43 +0300

    Add new object type 'ErrorStack'

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -9,3 +9,14 @@ export interface ViewProps {
 	baseUrl: string,
 	stylingRaw: string
 }
+
+export interface ErrorStack {
+	fileName: string,
+	lineNumber: number,
+	functionName: string,
+	typeName: string,
+	methodName: string,
+	columnNumber: number,
+	native: boolean,
+	codeRaw: string[]
+}
