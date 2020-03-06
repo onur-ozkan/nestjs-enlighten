47836c51ea2b3f96e816ee587a71a279c19f6519
ozkanonur <onurozkan.dev@outlook.com>
2020-03-06 21:15:57 +0300

    Get exactly 15 below and above lines of error line

--- src/Services/Resolver/stack-resolver.ts
+++ src/Services/Resolver/stack-resolver.ts
@@ -16,7 +16,7 @@ export class StackResolverService {
 			// Resolve the exception line and add it to stack object as property.
 			const data = fs.readFileSync(errorStack[index].fileName, 'utf-8')
 			const lines = data.split(/\r?\n/);
-			errorStack[index].codeRaw = lines.slice((errorStack[index].lineNumber - 15) >= 0 ? errorStack[index].lineNumber - 15 : 0, errorStack[index].lineNumber + 15)
+			errorStack[index].codeRaw = lines.slice((errorStack[index].lineNumber - 16) >= 0 ? errorStack[index].lineNumber - 16 : 0, errorStack[index].lineNumber + 15)
 
 			// Update absolute paths with relative ones.
 			const relativeFileName = errorStack[index].getFileName().indexOf(process.cwd())
