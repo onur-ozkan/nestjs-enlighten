e6061addbdb734a497b79872f7cc25b15304d91a
ozkanonur <onurozkan.dev@outlook.com>
2020-03-09 13:22:09 +0300

    Define new types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,13 +1,38 @@
-export interface ExceptionResponse {
-	error: string,
-	message: string,
-	statusCode: number
+import * as os from 'os'
+
+export interface NodeEnvironment {
+	name: string,
+	lts?: string,
+	sourceUrl?: string,
+	headersUrl?: string,
+	version?: string
 }
 
-export interface ViewProps {
-	response: ExceptionResponse,
-	baseUrl: string,
-	stylingRaw: string
+export interface RequestData {
+	headers: object,
+	method: object,
+	body: object,
+	query: object,
+}
+
+export interface ErrorState {
+	[key: string]: {
+		error: string,
+		info: string,
+		solutionOptions: {
+			intentional: string,
+			notIntentional: string
+		}
+	}
+}
+
+export interface DeterminedError {
+    error: string
+    info: string
+    solutionOptions: {
+        intentional: string
+        notIntentional: string
+    }
 }
 
 export interface ErrorStack {
@@ -19,4 +44,31 @@ export interface ErrorStack {
 	columnNumber: number,
 	native: boolean,
 	codeRaw: string[]
+}
+
+export interface MachineStack {
+	network: {
+		[index: string]: os.NetworkInterfaceInfo[];
+	},
+	cpu: {
+		model: string,
+		architecture: string,
+		thread: number
+	},
+	os: NodeJS.Platform,
+	nodeEnvironment: NodeEnvironment
+}
+
+export interface ViewProps {
+	response: any,
+	baseUrl: string,
+	projectPath: string,
+	errorStack: ErrorStack,
+	requestStack: RequestData,
+	environmentStack: object,
+	machineStack: MachineStack,
+	cssRaw: string,
+	prismJsRaw: Buffer,
+	errorObject: DeterminedError,
+	errorSolution: string,
 }
