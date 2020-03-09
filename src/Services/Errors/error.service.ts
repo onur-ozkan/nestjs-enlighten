505fde7b4c4f49e97c3c216dabf3642a34c402a5
ozkanonur <onurozkan.dev@outlook.com>
2020-03-09 13:28:26 +0300

    Update ViewCompiler and Error services

---
+++ src/Services/Errors/error.service.ts
@@ -0,0 +1,21 @@
+import { Errors } from './State'
+import { ErrorStack, DeterminedError } from '../../@types'
+
+import * as stackTrace from 'stack-trace'
+
+export class ErrorService {
+	private exception: { response: { statusCode: string | number } }
+
+	constructor(exception: { response: { statusCode: string | number } }) {
+		this.exception = exception
+	}
+
+	public errorDeterminator(): DeterminedError {
+		return Errors[this.exception.response.statusCode]
+	}
+
+	public getSolution(errorObject: DeterminedError): string {
+		const errorStack: ErrorStack[] = stackTrace.parse(this.exception)
+		return (!errorStack[0].typeName || !errorStack[0].methodName) ? errorObject.solutionOptions.notIntentional : errorObject.solutionOptions.intentional
+	}
+}
---
+++ src/Services/ViewCompiler/view-compiler.service.ts
@@ -0,0 +1,48 @@
+import { ArgumentsHost } from '@nestjs/common'
+import { DeterminedError, ErrorStack, RequestData, MachineStack, ViewProps } from '../../@types'
+import { ErrorService } from '../Errors/error.service'
+import { StackResolverService } from '../Resolver'
+import { RequestResolverService } from '../Resolver'
+import { EnvironmentResolverService } from '../Resolver'
+import { MachineResolverService } from '../Resolver'
+
+import * as fs from 'fs'
+import * as ejs from 'ejs'
+
+export class ViewCompilerService {
+	private exception: any
+	private request: any
+
+	constructor(host: ArgumentsHost, exception: any) {
+		this.exception = exception
+		this.request = host.switchToHttp().getRequest()
+	}
+
+	public async getCompiledView(): Promise<ViewProps> {
+		const errorObject: DeterminedError = new ErrorService(this.exception).errorDeterminator()
+		const errorSolution: string = new ErrorService(this.exception).getSolution(errorObject)
+		const errorStack: ErrorStack = await new StackResolverService(this.exception).getProperErrorStack()
+		const requestStack: RequestData = new RequestResolverService(this.request).getRequestData()
+		const environmentStack: object = new EnvironmentResolverService().getEnvironmentList()
+		const machineStack: MachineStack = new MachineResolverService().getProperMachineStack()
+
+		const cssRaw: string = fs.readFileSync(`${__dirname}/../../../assets/style/index.css`, 'utf8')
+		const prismJsRaw: Buffer = fs.readFileSync(`${__dirname}/../../../assets/libs/prism-js/prism.min.js`)
+
+		const compiledView: ViewProps = ejs.render(fs.readFileSync(`${__dirname}/../../views/index.ejs`, 'utf8'), {
+			response: this.exception.response,
+			baseUrl: `${this.request.protocol}://${this.request.headers.host}/`,
+			projectPath: process.env.PWD,
+			errorStack,
+			requestStack,
+			environmentStack,
+			machineStack,
+			cssRaw,
+			prismJsRaw,
+			errorObject,
+			errorSolution,
+		})
+
+		return compiledView
+	}
+}
