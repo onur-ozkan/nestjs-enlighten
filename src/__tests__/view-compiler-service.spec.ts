4e4a905aeb78ae30de6e94bc5c95df30c0ebe095
ozkanonur <onurozkan.dev@outlook.com>
2020-03-14 16:34:35 +0300

    Update view compiler test and service

--- src/__tests__/view-compiler-service.spec.ts
+++ src/__tests__/view-compiler-service.spec.ts
@@ -3,7 +3,7 @@ import { ViewCompilerService } from '../Services/ViewCompiler'
 import { MockedRequest } from '../__mocks__'
 
 it('Checks if getProperMachineStack() returns proper object values', async () => {
-	const compileRaw = await new ViewCompilerService(MockedRequest, new NotFoundException()).getCompiledView()
+	const compileRaw = await new ViewCompilerService(MockedRequest, new NotFoundException()).getCompiledView('theme-light')
 	const parseDoctypeDecleration = compileRaw.match(/<!DOCTYPE html>/)
 	expect('<!DOCTYPE html>').toEqual(String(parseDoctypeDecleration))
 })
