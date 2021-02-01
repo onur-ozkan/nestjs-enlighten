66e8c7c303fedaafd82da9c27487734bd164bf41
Daniel Wetzel <mail@danielkbx.com>
2021-02-01 08:47:53 +0100

    Fixed crash when file content of node internal code is read. Changed exception filter to ignore non-http requests.

--- src/Services/Resolver/stack-resolver.ts
+++ src/Services/Resolver/stack-resolver.ts
@@ -15,7 +15,7 @@ export class StackResolverService {
 
 		await errorStack.map(async (_stack: any, index: string | number) => {
 			// Resolve the exception line and add it to stack object as property.
-			const data = fs.readFileSync(errorStack[index].fileName, 'utf-8')
+			const data = fs.existsSync(errorStack[index].fileName) ? fs.readFileSync(errorStack[index].fileName, 'utf-8') : '';
 			const lines = data.split(/\r?\n/);
 			errorStack[index].codeRaw = lines.slice((errorStack[index].lineNumber - 16) >= 0 ? errorStack[index].lineNumber - 16 : 0, errorStack[index].lineNumber + 15)
 
